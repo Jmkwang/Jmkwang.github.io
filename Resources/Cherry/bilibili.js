37562ff4bb1d4fac295082336bc3f6db09d59856
Jkwang <1923546727@qq.com>
2021-11-18 02:25:20 +0800

    .

---
+++ Resources/Cherry/bilibili.js
@@ -0,0 +1,33 @@
+//Bilibili fans number
+    //https%3A%2F%2Fapi.bilibili.com%2Fx%2Fweb-interface%2Fcard%3Fmid%3D471303350
+    //https://api.bilibili.com/x/space/upstat?mid=471303350
+
+
+
+    // function show_bilibili_info(dataArray){
+    //     console.log(dataArray)
+    //     //console.log(dataArray.data.card.Official.title, dataArray.data.card.fans, dataArray.data.card.name, dataArray.data.like_num)
+    //     document.write(" fans_num: ", dataArray.data.card.fans, " Like_num: ", dataArray.data.like_num)
+    // }
+
+    // function show_bilibili_view(viewNum){
+
+    //     document.write(" view_num: ", dataArray.data.card.fans, " Like_num: ", dataArray.data.like_num)
+    // }
+    // setInterval(show_bilibili_fans, 120000);
+
+    // function saving_data(){
+    //     function get_time(){
+    //         let timeArr = timeHandler(new Date());
+    //     }
+    //     for(var hour_count = 0; eternal < 24; hour_count++){
+            
+    //     }
+    // }
+
+    $.get('https://jsonp.afeld.me/?url=https%3A%2F%2Fapi.bilibili.com%2Fx%2Fweb-interface%2Fcard%3Fmid%3D471303350', function(dataOne){
+        console.log(dataOne);
+        });
+    $.get('https://jsonp.afeld.me/?url=https://api.bilibili.com/x/space/upstat?mid=471303350', function(dataTwo){
+        console.log(dataTwo);
+    });
